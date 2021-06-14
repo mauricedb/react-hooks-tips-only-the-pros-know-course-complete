5966344a89fa0f611204ed9996f7f69ad5a7536b
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-06-14 16:40:15 +0200

    The Kimrof context and provider

--- src/kimrof-user-editor/KimrofUserEditor.tsx
+++ src/kimrof-user-editor/KimrofUserEditor.tsx
@@ -1,11 +1,16 @@
 import React, { ReactElement } from "react"
 
-// import { initialPerson } from "../utils"
-// import { IndexedPerson } from "../types/IndexedPerson";
+import { initialPerson } from "../utils"
+import { IndexedPerson } from "../types/IndexedPerson"
 
 // Kimrof = Formik reversed :-)
 import { UserEditor } from "./UserEditor"
+import { Kimrof } from "./kimrof"
 
 export function KimrofUserEditor(): ReactElement {
-  return <UserEditor />
+  return (
+    <Kimrof initialValues={initialPerson as IndexedPerson}>
+      <UserEditor />
+    </Kimrof>
+  )
 }
--- src/kimrof-user-editor/kimrof/Kimrof.tsx
+++ src/kimrof-user-editor/kimrof/Kimrof.tsx
@@ -1,15 +1,24 @@
 // Kimrof = Formik reversed :-)
 
-import React, { ReactElement, ReactNode } from "react"
+import React, { ReactElement, ReactNode, useMemo } from "react"
 
 import { KimrofObject, KimrofProperty } from "./Types"
+import { KimrofContext, kimrofContext } from "./KimrofContext"
 
 interface Props<TData> {
   children: ReactNode
+  initialValues: TData
 }
 
 export function Kimrof<TData extends KimrofObject>({
   children,
+  initialValues,
 }: Props<TData>): ReactElement {
-  return <>{children}</>
+  const values = initialValues
+
+  const context: KimrofContext = useMemo(() => ({ values }), [values])
+
+  return (
+    <kimrofContext.Provider value={context}>{children}</kimrofContext.Provider>
+  )
 }
