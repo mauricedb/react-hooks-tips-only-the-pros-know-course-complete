7c2ae69ac0f2b9b635f90e1f15544d941a31cd46
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-06-16 13:04:39 +0200

    Displaying values

--- src/kimrof-user-editor/kimrof/KimrofLabeledField.tsx
+++ src/kimrof-user-editor/kimrof/KimrofLabeledField.tsx
@@ -1,15 +1,16 @@
-import React, { ReactElement } from "react"
+import React, { ComponentProps, ReactElement } from "react"
 
 import { LabeledInput } from "../../components"
+import { useKimrofField } from "./useKimrofField"
 
-interface Props
-  extends Omit<
-    React.ComponentProps<typeof LabeledInput>,
-    "onChange" | "value"
-  > {
+type LabeledInputProps = ComponentProps<typeof LabeledInput>
+
+interface Props extends Omit<LabeledInputProps, "onChange" | "value"> {
   name: string
 }
 
 export function KimrofLabeledField(props: Props): ReactElement {
-  return <LabeledInput {...props} />
+  const fieldProps = useKimrofField(props.name)
+
+  return <LabeledInput {...props} {...fieldProps} />
 }
--- src/kimrof-user-editor/kimrof/useKimrofField.ts
+++ src/kimrof-user-editor/kimrof/useKimrofField.ts
@@ -1,3 +1,16 @@
-import React from "react"
+import { ChangeEvent, useCallback, useContext } from "react"
 
-import { KimrofProperty } from "./Types"
+import { kimrofContext } from "./KimrofContext"
+
+export function useKimrofField(name: string) {
+  const { values } = useContext(kimrofContext)
+
+  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    // TODO
+  }, [])
+
+  return {
+    value: values[name],
+    onChange,
+  } as const
+}
